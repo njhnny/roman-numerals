87472c17a2bb32559043a6e181be5b6c1d08a596
Nick Hennessy <njhnny@gmail.com>
2020-10-29 14:09:35 -0700

    clean up js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,18 @@
 $(document).ready(function () {
   $("form#generator").submit(function (event) {
     event.preventDefault();
-
     let num = parseInt($("input#userNumber").val());
-    
-
-    //function convertToRoman(num) {
-      let numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
-          romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-          numRoman =  "";
-    
+    let numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+        romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+        numRoman =  "";
       for(let i=0; i<numbers.length; i++) {
         while(num>=numbers[i]){
           numRoman = numRoman + romanNumeral[i];
           num = num - numbers[i];
-          
         }
-        };
-     //document.getElementById("demo").innerHTML = numRoman;
+      };
     $("p").text(numRoman);
       });
-      
   });
 
 
